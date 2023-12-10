09734ea1b52cacab55d54deba9b4b08b5094c940
Daktic <gberg@DakticDigital.com>
2023-12-10 13:16:45 -0500

    fixed problem where List should have ben Link

--- src/first.rs
+++ src/first.rs
@@ -11,13 +11,24 @@ impl List {
     pub fn push(&mut self, elem:i32) {
         let new_node = Box::new(Node {
             elem,
+            // This works in the book, but needs a different implementation.
+            // Possible I also missed something in the book.
             next: mem::replace(&mut self.head, Link::Empty),
         });
 
         self.head = Link::More(new_node);
     }
     pub fn pop(&mut self) -> Option<i32> {
-        // TODO
+        let result;
+        match &self.head {
+            Link::Empty => {
+              result = None;
+            }
+            Link::More(node) => {
+                self.head = node.next;
+            }
+        };
+        unimplemented!("TODO")
     }
 }
 
@@ -27,5 +38,5 @@ enum Link {
 }
 struct Node {
     elem: i32,
-    next: List,
+    next: Link,
 }
