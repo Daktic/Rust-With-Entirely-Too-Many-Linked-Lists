de716da7af7a5ca31b72f8d643c0acb80260a24b
Daktic <gberg@DakticDigital.com>
2023-12-14 22:21:35 -0500

    Trying to create push_front method, but IDK what struct it needs to reference.

--- src/fourth.rs
+++ src/fourth.rs
@@ -31,4 +31,22 @@ impl<T> List<T> {
             tail: None,
         }
     }
+
+    // Is this where I put this?
+    pub fn push_front(&mut self, elem: T) {
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                // Non-empty list. Need to connect the old_head to the new_head
+                old_head.prev = Some(new_head.clone());
+                new_head.next = Some(old_head);
+                self.head = Some(new_head);
+            }
+            None => {
+                // Empty list. Need to set the tail
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            }
+        }
+    }
 }
